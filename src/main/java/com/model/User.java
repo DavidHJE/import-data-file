adbeca861c392a5f6f4ec365762c81d824980d7e
David <dadahuet13@gmail.com>
2021-08-11 15:03:45 +0400

    add inscriptionForm

---
+++ src/main/java/com/model/User.java
@@ -0,0 +1,38 @@
+package com.model;
+
+public class User {
+
+	private Integer id;
+	private String name;
+	private String email;
+
+	public User(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
