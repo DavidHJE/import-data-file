51e7121a3de6ecc1ab1ecf79058f3f40ead61283
David <dadahuet13@gmail.com>
2021-08-11 15:53:22 +0400

    add component to the form

--- src/main/java/com/view/ConnectionForm.java
+++ src/main/java/com/view/ConnectionForm.java
@@ -1,15 +1,33 @@
 package com.view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Cursor;
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 
 public class ConnectionForm extends JFrame {
 
 	private JPanel contentPane;
+	
+	private JButton btnInscription;
+	private JLabel mailLabel;
+	private JTextField mailField;
+	private JLabel passwordLabel;
+	private JPasswordField passwordField;
+	private JButton btnConnexion;
+
+	private JLabel connexionLink;
 
 	/**
 	 * Launch the application.
@@ -31,12 +49,45 @@ public class ConnectionForm extends JFrame {
 	 * Create the frame.
 	 */
 	public ConnectionForm() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setResizable(false);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setSize(600, 450);
+		setLocationRelativeTo(null);
+		
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setLayout(null);
 		setContentPane(contentPane);
+		
+		btnInscription = new JButton("S'inscrire");
+		btnInscription.setBounds(400, 30, 150, 40);
+		contentPane.add(btnInscription);
+		
+		mailLabel = new JLabel("E-mail");
+		mailLabel.setBounds(125, 125, 150, 30);
+		contentPane.add(mailLabel);
+
+		mailField = new JTextField();
+		mailField.setBounds(275, 125, 200, 30);
+		contentPane.add(mailField);
+		
+		passwordLabel = new JLabel("Mot de passe");
+		passwordLabel.setBounds(125, 195, 150, 30);
+		contentPane.add(passwordLabel);
+
+		passwordField = new JPasswordField();
+		passwordField.setBounds(275, 195, 200, 30);
+		contentPane.add(passwordField);
+		
+		btnConnexion = new JButton("Se connecter");
+		btnConnexion.setBounds(225, 270, 150, 40);
+		contentPane.add(btnConnexion);
+
+		connexionLink = new JLabel("Mot de passe oublié ?");
+		connexionLink.setHorizontalAlignment(SwingConstants.CENTER);
+		connexionLink.setForeground(Color.blue.darker());
+		connexionLink.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		connexionLink.setBounds(200, 320, 200, 30);
+		contentPane.add(connexionLink);
 	}
 
 }
