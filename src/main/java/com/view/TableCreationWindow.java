df5c5df10eaad60fe37907bb28415af2b92a7543
Dylan Domen <dylan.domen@gmail.com>
2021-08-16 16:36:55 +0100

    Table for imports fields (update,checkbox) + function create table(beginning)

--- src/main/java/com/view/TableCreationWindow.java
+++ src/main/java/com/view/TableCreationWindow.java
@@ -9,6 +9,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.DefaultCellEditor;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 
 import java.awt.event.ActionListener;
@@ -52,13 +53,13 @@ public class TableCreationWindow extends JFrame {
 	
 	public TableCreationWindow (final Integer nbColumn, final String columnNames) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 646, 403);
+		setBounds(100, 100, 1000, 900);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
 		JButton BtnReturn = new JButton("Retour");
-		BtnReturn.setBounds(10, 11, 89, 23);
+		BtnReturn.setBounds(20, 11, 89, 23);
 		BtnReturn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				MainWindow mainWindow = new MainWindow();
@@ -70,7 +71,7 @@ public class TableCreationWindow extends JFrame {
 		contentPane.add(BtnReturn);
 		
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(50, 45, 420, 235);
+		scrollPane.setBounds(20, 45, 742, 354);
 		contentPane.add(scrollPane);
 		
 		TableImportfields = new JTable();
@@ -78,31 +79,56 @@ public class TableCreationWindow extends JFrame {
 			new Object[][] {
 			},
 			new String[] {
-				"COL CSV", "COL TABLE"
+				"Champ fichier", "Champ table", "Identifiant unique", "Doit \u00EAtre unique", "Peut \u00EAtre null ?", "Type", ""
 			}
 		));
 		scrollPane.setViewportView(TableImportfields);
 		
-		JButton BtnTestCombo = new JButton("New button");
-		BtnTestCombo.addActionListener(new ActionListener() {
+		JButton btnNewButton = new JButton("Test Lecture Table");
+		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				DefaultTableModel tableModel = (DefaultTableModel) TableImportfields.getModel();
-				
-				String column[] = columnNames.split(";");
-				String[]elementsCombo = new String[nbColumn];
-				
-		        for (int i = 0; i < column.length; i++) {
-		        	elementsCombo[i] = column[i]; 
-		        }
-				JComboBox combo = new JComboBox(elementsCombo);
-				TableImportfields.getColumnModel().getColumn(0).setCellEditor(new DefaultCellEditor(combo));
-				
-				for(int i = 0 ; i<nbColumn ; i++) {
-					tableModel.addRow(new Objects[] {});
-				}
+				CreateTable();
 			}
 		});
-		BtnTestCombo.setBounds(381, 11, 89, 23);
-		contentPane.add(BtnTestCombo);
+		btnNewButton.setBounds(604, 11, 158, 23);
+		contentPane.add(btnNewButton);
+		
+		ShowTable(nbColumn,columnNames);
+		
+	}
+	
+	public void ShowTable(final Integer nbColumn, final String columnNames) {
+		DefaultTableModel tableModel = (DefaultTableModel) TableImportfields.getModel();
+		
+		String column[] = columnNames.split(";");
+		String[]elementsCombo = new String[nbColumn];
+		String[]elementsComboType = {"Nombre","Texte","Vrai/Faux"};
+        for (int i = 0; i < column.length; i++) {
+        	elementsCombo[i] = column[i]; 
+        }
+		JComboBox combo = new JComboBox(elementsCombo);
+		JComboBox comboType = new JComboBox(elementsComboType);
+		JCheckBox checkBox = new JCheckBox();
+		
+		TableImportfields.getColumnModel().getColumn(0).setCellEditor(new DefaultCellEditor(combo));
+		TableImportfields.getColumnModel().getColumn(5).setCellEditor(new DefaultCellEditor(comboType));
+		TableImportfields.getColumnModel().getColumn(2).setCellEditor(new DefaultCellEditor(checkBox));
+		TableImportfields.getColumnModel().getColumn(3).setCellEditor(new DefaultCellEditor(checkBox));
+		TableImportfields.getColumnModel().getColumn(4).setCellEditor(new DefaultCellEditor(checkBox));
+		
+		for(int i = 0 ; i<nbColumn ; i++) {
+			tableModel.addRow(new Objects[] {});
+		}
+	}
+	
+	public void CreateTable() {
+		//lecture de la table
+		for(int i = 0; i<TableImportfields.getModel().getRowCount() ; i++) {
+			for(int j = 0; j<((TableImportfields.getModel().getColumnCount())-1) ; j++) {
+				String cellInfo = TableImportfields.getValueAt(i,j).toString();
+				System.out.println(cellInfo);
+			}
+			System.out.println("======================================");
+		}
 	}
 }
