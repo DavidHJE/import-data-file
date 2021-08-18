3eee15721a3cbc941a35d265d8bfd3c006578f60
Dylan Domen <dylan.domen@gmail.com>
2021-08-18 10:28:35 +0100

    create string for script

--- src/main/java/com/view/TableCreationWindow.java
+++ src/main/java/com/view/TableCreationWindow.java
@@ -18,16 +18,21 @@ import java.util.Vector;
 import java.awt.event.ActionEvent;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class TableCreationWindow extends JFrame {
 
 	private JPanel contentPane;
 	private JTable TableImportfields;
 	private JScrollPane scrollPane;
+	private JTextField TableNameField;
 	
 	String absolutepath;
 	static Integer nbColumn;
 	static String columnNames = "";
+	
 	
 	
 	/**
@@ -87,22 +92,31 @@ public class TableCreationWindow extends JFrame {
 		JButton btnNewButton = new JButton("Test Lecture Table");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				CreateTable();
+				CreateTable(nbColumn,columnNames);
 			}
 		});
 		btnNewButton.setBounds(604, 11, 158, 23);
 		contentPane.add(btnNewButton);
 		
+		TableNameField = new JTextField();
+		TableNameField.setBounds(277, 12, 183, 20);
+		contentPane.add(TableNameField);
+		TableNameField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Nom de la table");
+		lblNewLabel.setBounds(186, 15, 81, 14);
+		contentPane.add(lblNewLabel);
+		
 		ShowTable(nbColumn,columnNames);
 		
 	}
 	
-	public void ShowTable(final Integer nbColumn, final String columnNames) {
+	public void ShowTable(Integer nbColumn, String columnNames) {
 		DefaultTableModel tableModel = (DefaultTableModel) TableImportfields.getModel();
 		
 		String column[] = columnNames.split(";");
 		String[]elementsCombo = new String[nbColumn];
-		String[]elementsComboType = {"Nombre","Texte","Vrai/Faux"};
+		String[]elementsComboType = {"Nombre","Nombre à virgule","Texte","Vrai/Faux"};
         for (int i = 0; i < column.length; i++) {
         	elementsCombo[i] = column[i]; 
         }
@@ -121,14 +135,74 @@ public class TableCreationWindow extends JFrame {
 		}
 	}
 	
-	public void CreateTable() {
-		//lecture de la table
-		for(int i = 0; i<TableImportfields.getModel().getRowCount() ; i++) {
-			for(int j = 0; j<((TableImportfields.getModel().getColumnCount())-1) ; j++) {
-				String cellInfo = TableImportfields.getValueAt(i,j).toString();
-				System.out.println(cellInfo);
-			}
-			System.out.println("======================================");
+	public void CreateTable(Integer nbColumn, String columnNames) {
+		
+		String TableName = TableNameField.getText();
+		Integer start = 0;
+		Boolean continueCreation = true;
+		String finalSQL = "CREATE TABLE [IF NOT EXISTS] ";
+		String type;
+		if(TableName.equals("")) {
+//			JOptionPane.showMessageDialog(Message, "ERREUR!\nVeuillez remplir correctement le nom de la table",
+//                    "Erreur", JOptionPane.ERROR_MESSAGE);
+			System.out.println("erreur nom table vide");
 		}
+		else {
+			finalSQL += TableName+" (";
+			for(int i = 0; i < TableImportfields.getModel().getRowCount() ; i++) {
+				for(int j = 1; j<((TableImportfields.getModel().getColumnCount())-1) ; j++) {
+					String cellInfo = TableImportfields.getValueAt(i,j).toString();
+					type = TableImportfields.getValueAt(i,5).toString();
+					switch(type) {//type
+					case"Nombre":
+						type = "INT ";
+						break;
+					case"Nombre à virgule":
+						type = "numeric ";
+						break;
+					case"Texte":
+						type = "VARCHAR (255) ";
+						break;
+					case"Vrai/Faux":
+						type = "boolean ";
+						break;
+					default:
+						continueCreation = false;
+					}
+					
+					switch(j) {
+						case 1:
+							finalSQL += cellInfo + " "+ type;
+							break;
+						case 2:
+							if(cellInfo.equals("true")) {
+								finalSQL += "serial PRIMARY KEY ";
+							}
+							break;
+						case 3:
+							if(cellInfo.equals("true")) {
+								finalSQL += "UNIQUE ";
+							}
+							break;
+						case 4:
+							if(cellInfo.equals("true")) {
+								finalSQL += "NULL,";
+							}else if(cellInfo.equals("false")){
+								finalSQL += "NOT NULL,";
+							}else {
+								continueCreation = false;
+							}
+							break;
+						default:
+							
+					}
+				}
+			}
+			finalSQL += ");";
+			if(continueCreation == true) {
+				System.out.println(finalSQL);
+			}
+		}
+		
 	}
 }
